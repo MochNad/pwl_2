277adf3cbfbf8645e360e3ab276e45320ec1509b
Moch. Nadi Rafli Maulana <mochammadnadiraflimaulana@gmail.com>
2023-02-20 09:59:28 +0700

    praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "Selamat Datang";
-});
+// Praktikum 1
 
-Route::get('/about', function () {
-    return "NIM : 2141720188, Nama : Moch. Nadi Rafli Maulana";
-});
+// Route::get('/', function () {
+//     return "Selamat Datang";
+// });
 
-Route::get('/articles/{id}', function ($id) {
-    return "Halaman Artikel ID : $id";
-});
+// Route::get('/about', function () {
+//     return "NIM : 2141720188, Nama : Moch. Nadi Rafli Maulana";
+// });
+
+// Route::get('/articles/{id}', function ($id) {
+//     return "Halaman Artikel ID : $id";
+// });
+
+// Praktikum 2
+
+// Route::get('/', [PageController::class, 'index']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
